001eae1b8b77bb36555d7903975ad7d20f92a26f
tuxjs <omarlg@unica.cu>
2017-08-04 18:51:28 -0400

    testing the new nodes features

--- tests/node.test.js
+++ tests/node.test.js
@@ -1,5 +1,5 @@
 describe("Node Tests", () => {
-  const {createNode} = require("node");
+  const {createNode} = require("Node");
 
   it("should export a createNode function", () => {
     expect(createNode).toBeDefined();
@@ -7,9 +7,8 @@ describe("Node Tests", () => {
 
   describe("createNode function", () => {
 
-    const newNode = createNode();
-
     it("should create a new node with leftChild,rightChild and parentNode equal to undefined",() => {
+        const newNode = createNode();
         expect(newNode).toMatchObject({
           leftChild: undefined,
           rightChild: undefined,
@@ -18,21 +17,39 @@ describe("Node Tests", () => {
     });
 
     it("should create a new node with the 'key' property private",() => {
+      const newNode = createNode();
       expect(newNode.hasOwnProperty('key')).toBeFalsy();
-
     });
 
     it("should setup the 'key' property equal to 0 by default",() => {
+      const newNode = createNode();
       expect(newNode.getKey()).toBe(0);
     });
 
     it("should config getter and setter for the key property",() => {
-      expect(newNode.getKey).toBeDefined();
-      expect(newNode.getKey).toBeInstanceOf(Function);
-      expect(newNode.setKey).toBeDefined();
-      expect(newNode.setKey).toBeInstanceOf(Function);
+      const newNode = createNode();
       newNode.setKey(10);
       expect(newNode.getKey()).toBe(10);
+    });
+
+    test("every node is capable of setup correctly its children using setter function",() => {
+      const newNode = createNode();
+      newNode.setLeftChild(1);
+      expect(newNode.leftChild.getKey()).toBe(1);
+      newNode.setRightChild(2);
+      expect(newNode.rightChild.getKey()).toBe(2);
+    });
+
+    test("every node is capable of tell if it have children using hasChildren() function",() => {
+      const newNode = createNode(10);
+      expect(newNode.hasChildrens()).toBeFalsy();
+      newNode.setRightChild(20);
+      expect(newNode.hasChildrens()).toBeTruthy();
+    });
+
+    test("every node is capable of return its childrens",() => {
+      const newNode = createNode();
+      expect(newNode.childrens()).toMatchObject({leftChild: undefined,rightChild: undefined});
     });
   });
 });
