5ce17cf10b96e14be049b5d6aad5f41c38a99ad4
tuxjs <omarlg@unica.cu>
2017-08-08 23:46:53 -0400

    Implementing a more dinamyc insert function
    This function is capable of insert just one node or varius in the same
    call

--- src/Tree.js
+++ src/Tree.js
@@ -10,25 +10,34 @@ function addNode(value, treeNode) {
 
 function findNode(value, rootNode) {
   if (rootNode === undefined) {
-    return false;
+    return null;
   } else {
     const nodeValue = rootNode.getKey();
     return nodeValue === value
-      ? true
+      ? nodeValue
       : nodeValue < value
         ? findNode(value, rootNode.rightChild)
         : findNode(value, rootNode.leftChild);
   }
 }
 
+const isAListThis = Array.isArray;
+
 /* Declaring tree prototype */
 const Tree = {
   setRootNodeWith(value) {
     this.rootNode = createNode(value);
   },
-  insert(value) {
+  insert(...args) {
+    args.forEach(arg => {
+      isAListThis(arg) ? this.insertList(arg) : this.add(arg);
+    });
+  },
+  insertList(values) {
+    values.forEach(v => this.add(v));
+  },
+  add(value) {
     const { rootNode } = this;
-
     rootNode === undefined
       ? this.setRootNodeWith(value)
       : addNode(value, rootNode);
