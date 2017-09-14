fd8fb7c832e9868c320da69a1b6197230f3a811f
tuxjs <omarlg@unica.cu>
2017-09-14 15:23:26 -0400

    Declaring errors for node structure

---
+++ src/structures/Node/errors.js
@@ -0,0 +1,11 @@
+const {buildError} = require('../../utils/tools');
+
+/* Errors available for node structure */
+const missingNodeValue = buildError(
+  'MissingNodeValue',
+  'The node value is required for node creation'
+);
+
+module.exports = {
+  missingNodeValue
+};
