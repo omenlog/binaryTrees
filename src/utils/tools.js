1cb7e22c863d95b2b813dec35dd4465c3eebc418
tuxjs <omarlg@unica.cu>
2017-09-14 15:21:03 -0400

    Add buildError and launch functions to tools available
    the launch function just throw a new error using and error function
    passed as arguments
    
    the buildError function receive a name and a message for and return a
    new error function that can be used by throw and by the launch function

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -6,6 +6,27 @@ function flat(args) {
   return args.reduce(flatReducer, []);
 }
 
+/* function to build another error functions*/
+function buildError(errorName, errorMessage) {
+  const errorFunction = function errorFunction() {
+    this.name = errorName;
+    this.message = errorMessage;
+    this.stack = new Error().stack;
+  };
+
+  errorFunction.name = 'errorFunction';
+  errorFunction.prototype = Object.create(Error.prototype);
+  errorFunction.prototype.constructor = errorFunction;
+
+  return errorFunction;
+}
+
+const launch = ErrorFunction => {
+  throw new ErrorFunction();
+};
+
 module.exports = {
-  flat
+  flat,
+  buildError,
+  launch
 };
