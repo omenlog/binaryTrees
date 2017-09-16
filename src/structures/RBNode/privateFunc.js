7459265e6eb2571c400b02fe4db498efe0f6177f
tuxjs <omarlg@unica.cu>
2017-09-16 15:50:58 -0400

    Creating private functions for red black nodes
    Now the only function available is a function that allow build node
    prototype

---
+++ src/structures/RBNode/privateFunc.js
@@ -0,0 +1,10 @@
+const rbNodePrototype = require('./prototype');
+const {createNode} = require('../Node/index');
+
+function buildNodePrototype(nodeKey){
+  return Object.assign(createNode(nodeKey), rbNodePrototype);
+}
+
+module.exports = {
+  buildNodePrototype
+};
