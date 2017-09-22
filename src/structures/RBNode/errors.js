61ebf383f0493e390a93afb1d0550166567bff8e
tuxjs <omarlg@unica.cu>
2017-09-21 22:45:37 -0400

    Creating and exporting error function unableToMakeRightRotation

--- src/structures/RBNode/errors.js
+++ src/structures/RBNode/errors.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const {buildError} = require('../../utils/tools');
 
 const unableToMakeLeftRotation = buildError(
@@ -5,6 +7,14 @@ const unableToMakeLeftRotation = buildError(
   "Over this node can't be exectuted a left rotation because it hasn't right child and this child is mandatory for this procedure"
 
 );
+
+const unableToMakeRightRotation = buildError(
+  'UnableToMakeLeftRotation',
+  "Over this node can't be exectuted a right rotation because it hasn't left child and this child is mandatory for this procedure"
+
+);
+
 module.exports = {
+  unableToMakeRightRotation,
   unableToMakeLeftRotation
 };
