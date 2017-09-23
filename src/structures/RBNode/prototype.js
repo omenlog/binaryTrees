76af1c39daeac14be47d8d21b9a86d9166975bdf
tuxjs <omarlg@unica.cu>
2017-09-23 17:34:23 -0400

    Implementing fixTheTree function
    This is the function that is fired after that one insertion is made in
    the red black tree and it objective is change the tree structure to
    reach the 5 conditions necessary for red black trees

--- src/structures/RBNode/prototype.js
+++ src/structures/RBNode/prototype.js
@@ -54,6 +54,48 @@ const rbNodePrototype = {
       nodeLeftChild.parentNode = this.parentNode;
       this.parentNode = nodeLeftChild;
     }
+  },
+  fixTheTree(){
+    let z = this;
+    while(z.parentNode && z.parentNode.getColor() === 'RED'){
+      if(z.parentNode.isALeftChild()){
+        const y = z.parentNode.parentNode.rightChild;
+        if(y && y.getColor() === 'RED'){                   // case 1
+          z.parentNode.setColor('BLACK');             // case 1
+          y.setColor('BLACK');                        // case 1
+          z.parentNode.parentNode.setColor('RED');    // case 1
+          z = z.parentNode.parentNode                 // case 1
+        }
+        else{
+          if(!z.isALeftChild()){  // case 2
+              z = z.parentNode;          // case 2
+              z.rotateToLeft();          // case 2
+            }
+
+            z.parentNode.setColor('BLACK');          // case 3
+            z.parentNode.parentNode.setColor('RED'); // case 3
+            z.parentNode.parentNode.rotateToRight(); // case 3
+        }
+      }
+      else{
+        const y = z.parentNode.parentNode.leftChild;
+        if(y && y.getColor() === 'RED'){                   // case 1
+          z.parentNode.setColor('BLACK');             // case 1
+          y.setColor('BLACK');                        // case 1
+          z.parentNode.parentNode.setColor('RED');    // case 1
+          z = z.parentNode.parentNode                 // case 1
+        }
+        else{
+          if(z.isALeftChild()){  // case 2
+            z = z.parentNode;          // case 2
+            z.rotateToRight();          // case 2
+          }
+          z.parentNode.setColor('BLACK');          // case 3
+          z.parentNode.parentNode.setColor('RED'); // case 3
+          z.parentNode.parentNode.rotateToLeft(); // case 3
+        }
+      }
+    }
   }
 };
 
