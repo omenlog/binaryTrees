134bedfd4d97a9bf3704f496e7c494452d5875a8
tuxjs <omarlg@unica.cu>
2017-09-25 11:55:32 -0400

    Creating red black tree prototype
    The insert function for red black trees was reimplemented and this new
    function use internally the insert function of BST prototype

---
+++ src/structures/RBT/prototype.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const prototypeBST = require('../BST/prototype');
+const {createRBNode} = require('../RBNode');
+const {updateRootOf} = require('./privateFunc');
+
+const prototypeRB = Object.assign(Object.create(prototypeBST),{
+  insert(newKey){
+    const newRBNode = createRBNode(newKey,'RED');
+    /* calling the insert function of BST prototype object */
+    Object.getPrototypeOf(this.__proto__).insert(newRBNode);
+    newRBNode.fixTheTree();
+    updateRootOf(this);
+    return this;
+  }
+});
+
+module.exports = prototypeRB;
