048aedbdf1c7889d74805cfe909d9a341d7b522f
tuxjs <omarlg@unica.cu>
2017-09-25 12:01:01 -0400

    Test that RBT can insert new nodes and keep the tree structure correct

--- src/structures/RBT/RBTree.test.js
+++ src/structures/RBT/RBTree.test.js
@@ -6,4 +6,18 @@ describe('Red Black Tree tests',() => {
   it('should export a createRBT function',() => {
     expect(createRBT).toBeDefined();
   });
+
+  it('should create a new tree that allow introduce new nodes in the tree',() => {
+    const newRBTree = createRBT();
+    newRBTree.insert(1);
+    newRBTree.insert(3);
+    newRBTree.insert(2);
+
+    expect(newRBTree.rootNode.getKey()).toBe(2);
+    expect(newRBTree.rootNode.getColor()).toBe('BLACK');
+    expect(newRBTree.rootNode.leftChild.getKey()).toBe(1);
+    expect(newRBTree.rootNode.rightChild.getKey()).toBe(3);
+    expect(newRBTree.rootNode.leftChild.getColor()).toBe('RED');
+    expect(newRBTree.rootNode.rightChild.getColor()).toBe('RED');
+  });
 });
