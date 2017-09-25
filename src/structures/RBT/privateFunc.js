e33850b00d37e58cc7cd1d112a5c3f75c9e0859e
tuxjs <omarlg@unica.cu>
2017-09-25 11:52:46 -0400

    Creatin

---
+++ src/structures/RBT/privateFunc.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function updateRootNode(posibleRoot) {
+  return posibleRoot.parentNode === undefined
+    ? posibleRoot
+    : updateRootNode(posibleRoot.parentNode);
+}
+
+function updateRootOf(tree) {
+  let newRootNode = updateRootNode(tree.rootNode);
+  newRootNode.setColor('BLACK');
+  tree.rootNode = newRootNode;
+}
+
+module.exports = {
+  updateRootOf
+};
