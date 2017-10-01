3a31540b532d729e4a61c3c06607ed625b85bdd3
tuxjs <omarlg@unica.cu>
2017-10-01 00:13:47 -0400

    Fixing small bug on error creation for nodes and red black nodes

--- src/structures/RBNode/errors.js
+++ src/structures/RBNode/errors.js
@@ -4,13 +4,13 @@ const {buildError} = require("../../utils/tools");
 
 const unableToMakeLeftRotation = buildError(
   "UnableToMakeLeftRotation",
-  "Over this node can"t be exectuted a left rotation because it hasn"t right child and this child is mandatory for this procedure"
+  "Over this node can't be exectuted a left rotation because it hasn't right child and this child is mandatory for this procedure"
 
 );
 
 const unableToMakeRightRotation = buildError(
   "UnableToMakeLeftRotation",
-  "Over this node can"t be exectuted a right rotation because it hasn"t left child and this child is mandatory for this procedure"
+  "Over this node can't be exectuted a right rotation because it hasn't left child and this child is mandatory for this procedure"
 
 );
 
