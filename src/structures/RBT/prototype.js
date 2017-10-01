6093bda11a3d31bba1fa4440cfea166c50e988b7
tuxjs <omarlg@unica.cu>
2017-10-01 00:46:17 -0400

    Calling the insert function of BST using the call function
    this is necessary to avoid loose the link existing between the `this`
    parameter and the red black tree

--- src/structures/RBT/prototype.js
+++ src/structures/RBT/prototype.js
@@ -8,7 +8,7 @@ const prototypeRB = Object.assign(Object.create(prototypeBST),{
   insert(newKey){
     const newRBNode = createRBNode(newKey,"RED");
     /* calling the insert function of BST prototype object */
-    Object.getPrototypeOf(this.__proto__).insert(newRBNode);
+    Object.getPrototypeOf(this.__proto__).insert.call(this,newRBNode);
     newRBNode.fixTheTree();
     updateRootOf(this);
     return this;
