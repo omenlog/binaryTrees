66559f001e8f3429a0f5b4477b6c0ede5cee1911
tuxjs <omarlg@unica.cu>
2017-11-17 23:46:43 -0500

    Reimplementing remove function of RBT prototype
    Now with this new implementation if the user try to remove a value not
    present in the tree then this remove function do nothing and the tree
    remain unchanged

--- src/structures/RBT/prototype.js
+++ src/structures/RBT/prototype.js
@@ -2,9 +2,9 @@
 
 const prototypeBST = require('../BST/prototype');
 const { createRBNode } = require('../RBNode');
-const { addIn, deleteFixUp } = require('./privateFunc');
+const { addIn, removeFrom } = require('./privateFunc');
 const { isANodeThis } = require('../Node');
-const { flat, replaceIn, minOf } = require('../../utils/tools');
+const { flat } = require('../../utils/tools');
 
 const createRBNodes = arg => {
   return isANodeThis(arg) ? arg : createRBNode(arg, 'RED');
@@ -19,37 +19,8 @@ const prototypeRB = Object.assign(Object.create(prototypeBST), {
     return this;
   },
   remove(nodeKey) {
-    let x;
     const node = this.find(nodeKey);
-
-    let nodeOriginalColor = node.getColor();
-
-    if (!node.hasChildrens()) {
-      x = node.rightChild;
-      replaceIn(this, node, node.rightChild);
-    } else if (node.leftChild.isALeaf() || node.rightChild.isALeaf()) {
-      const nodeChild = node.leftChild.isALeaf()
-        ? node.rightChild
-        : node.leftChild;
-      x = nodeChild;
-      replaceIn(this, node, nodeChild);
-    } else {
-      const succesor = minOf(node.rightChild);
-      node.setKey(succesor.getValue());
-      nodeOriginalColor = succesor.getColor();
-      x = succesor.rightChild;
-      replaceIn(this, succesor, succesor.rightChild);
-    }
-
-    if (nodeOriginalColor === 'BLACK') {
-      if (x.isALeaf() && x === this.rootNode) {
-        this.rootNode = undefined;
-      } else {
-        deleteFixUp(this, x);
-      }
-    }
-
-    return this;
+    return node !== undefined ? removeFrom(this, node) : this;
   }
 });
 
